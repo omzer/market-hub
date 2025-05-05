cbeb74b2e55dfe456581508bfe8f7bf53b982a7f
o.sabri@freightos.com <o.sabri@freightos.com>
2025-05-05 18:46:46 +0300

    change

--- lib/src/view/widget/product/product_image_gallery.dart
+++ lib/src/view/widget/product/product_image_gallery.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
-class ProductImageGallery extends StatelessWidget {
+class ProductImageGallery extends StatefulWidget {
   final List<String> images;
   final double height;
   final Color backgroundColor;
@@ -13,24 +15,86 @@ class ProductImageGallery extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<ProductImageGallery> createState() => _ProductImageGalleryState();
+}
+
+class _ProductImageGalleryState extends State<ProductImageGallery> {
+  int _currentIndex = 0;
+  final CarouselController _controller = CarouselController();
+
+  @override
   Widget build(BuildContext context) {
     return Container(
-      height: height,
-      color: backgroundColor,
-      child: images.isNotEmpty
-          ? PageView.builder(
-              itemCount: images.length,
-              itemBuilder: (context, index) {
-                return Image.network(
-                  images[index],
-                  fit: BoxFit.contain,
-                  errorBuilder: (context, error, stackTrace) {
-                    return _buildErrorWidget();
-                  },
-                );
-              },
+      height: widget.height,
+      child: widget.images.isNotEmpty
+          ? Column(
+              children: [
+                Expanded(
+                  child: CarouselSlider.builder(
+                    itemCount: widget.images.length,
+                    options: CarouselOptions(
+                      height: widget.height - 40,
+                      viewportFraction: 1.0,
+                      enlargeCenterPage: true,
+                      enableInfiniteScroll: widget.images.length > 1,
+                      autoPlay: widget.images.length > 1,
+                      autoPlayInterval: const Duration(seconds: 4),
+                      autoPlayAnimationDuration:
+                          const Duration(milliseconds: 800),
+                      autoPlayCurve: Curves.fastOutSlowIn,
+                      onPageChanged: (index, reason) {
+                        setState(() {
+                          _currentIndex = index;
+                        });
+                      },
+                    ),
+                    itemBuilder: (context, index, realIndex) {
+                      return _buildImageItem(widget.images[index]);
+                    },
+                  ),
+                ),
+                if (widget.images.length > 1)
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 10.0),
+                    child: AnimatedSmoothIndicator(
+                      activeIndex: _currentIndex,
+                      count: widget.images.length,
+                      effect: const ExpandingDotsEffect(
+                        dotHeight: 8,
+                        dotWidth: 8,
+                        activeDotColor: Colors.black,
+                        dotColor: Colors.grey,
+                        spacing: 8,
+                      ),
+                      onDotClicked: (index) {
+                        setState(() {
+                          _currentIndex = index;
+                        });
+                      },
+                    ),
+                  ),
+              ],
             )
           : _buildErrorWidget(),
+    );
+  }
+
+  Widget _buildImageItem(String imageUrl) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 5.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(8.0),
+        child: Image.network(
+          imageUrl,
+          fit: BoxFit.contain,
+          errorBuilder: (context, error, stackTrace) {
+            return _buildErrorWidget();
+          },
+        ),
+      ),
     );
   }
 
